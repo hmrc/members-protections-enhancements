20bdf1bf87e69fb3b2d8e76cd3f7dda76ce23072
BetaDraconis <47660116+BetaDraconis@users.noreply.github.com>
2025-08-14 10:43:51 +0100

    LPE-821: Update all required dependencies. Remove any unused dependencies.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,24 +1,18 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "org.typelevel"                 %% "cats-core"                  % "2.12.0",
+    "org.typelevel"                 %% "cats-core"                  % "2.13.0",
     "com.chuusai"                   %% "shapeless"                  % "2.4.0-M1",
-    "com.networknt"                 %  "json-schema-validator"      % "1.5.3" exclude ("org.slf4j", "slf4j-api"),
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.18.1"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.19.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-30"      % bootstrapVersion,
-    "org.scalatestplus"            %% "scalacheck-1-18"             % "3.2.19.0",
-    "com.softwaremill.diffx"       %% "diffx-scalatest-should"      % "0.9.0"
   ).map(_ % Test)
 
 }
